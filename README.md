c9371a3900054c185dbe41c83cd4a01cec4b6432
Rafaela Amiliategui <amiliateguirafaela@gmail.com>
2026-05-07 11:07:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Projetos em Java
+
+Repositório dedicado aos meus estudos em java, Programação Orientada a Objetos, Desenvolvimento de lógica...
+
+## Obejtivo
+
+Aprofundar meus conhecimentos em java e evoluir como desenvolvedora Full Starck.
+
+## Dependency Management
+
+The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ src/Heranca/IA.java
@@ -0,0 +1,23 @@
+package Heranca;
+class Jarvis {
+    String comando;
+    public void ativar() {
+        System.err.println("Jarvis: Sim senhorita Rafaela, no que posso ajudar?");
+    }
+}
+class Usuario extends Jarvis {
+    public void ativar(){
+        super.ativar();
+        System.out.println("Ativar alarme de segurança.\nJarvis: Entendido. Sistema de alarme ativado!");
+    }
+}
+
+public class IA {
+    public static void main(String[] args) {
+        Usuario user = new Usuario();
+
+        user.comando = "Jarvis";
+        System.out.println("Hey " + user.comando);
+        user.ativar();
+    }
+}
