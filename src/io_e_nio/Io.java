4006990669d69eeac4421d868f017962c87133e6
Rafaela Amiliategui <amiliateguirafaela@gmail.com>
2026-06-15 18:39:23 -0300

    IO e NIO

---
+++ src/io_e_nio/Io.java
@@ -0,0 +1,26 @@
+package io_e_nio;
+import java.io.*;
+
+public class Io {
+    public static void main(String[] args) {
+        
+        try {
+            FileWriter writer = new FileWriter("C:\\Users\\oncel\\OneDrive\\Documentos\\Academico_java\\Projetos_java.txt");
+            writer.write("Olá, Rafaela");
+            
+            writer.close();
+
+            FileReader reader = new FileReader("C:\\Users\\oncel\\OneDrive\\Documentos\\Academico_java\\Projetos_java.txt");
+
+            int data = reader.read();
+
+            while (data != -1) {
+                System.out.println((char)data);
+                data = reader.read();
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Problemas de IO: " + e.getMessage());
+        }
+    }
+}
